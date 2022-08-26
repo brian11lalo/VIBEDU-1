bfaca9fa830cb1f736c3098775e013e0b1ddc1d1
Alberto Lopez <albertoojeda44@aragon.unam.mx>
2022-08-26 01:39:22 -0500

    init project ecommerce

---
+++ src/test/java/vibedu/ecommerce/VibeduApplicationTests.java
@@ -0,0 +1,13 @@
+package vibedu.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
